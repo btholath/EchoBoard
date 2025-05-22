87ab2afba65c96a6a9043510a54209ea7cbaba0f
Biju Tholath <54602395+btholath@users.noreply.github.com>
2025-05-22 23:31:49 +0000

    initial version

--- echoboard-front-end/vite.config.js
+++ echoboard-front-end/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // host: true ensures the server is accessible from Codespaces' browser preview or forwarded port.
+  server: {
+    host: true, // or '0.0.0.0'
+    port: 5173
+  }
 })
